7e5d7f107fc3e4727a771c89b23fb3d26acf9a59
Mauricio Luisก็็็็็็็็็็็็็็็็็็็็ก็็็็็็็็็็็็็็็็็็็็ <46418980+AWO52PZ44IKE7FDR79DV2999BCZK19R0IFLDGHF@users.noreply.github.com>
2021-08-08 04:45:02 -0500

    Add files via upload

---
+++ test.js
@@ -0,0 +1,8 @@
+with(document){
+          for(i in links){
+            links[i].onclick=function(){
+                  location.replace(this.href); 
+                  return false;
+                }
+          }
+        }
